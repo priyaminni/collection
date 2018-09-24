3b33c444bfd6805e14f7ffa508c8e6f11e1914e4
priya <priyanka.official9@gmail.com>
2018-09-24 14:13:23 -0500

    Changed duplicates

--- CoreJavaPractice/src/com/util/practice/RemoveDuplicate.java
+++ CoreJavaPractice/src/com/util/practice/RemoveDuplicate.java
@@ -7,7 +7,7 @@ public class RemoveDuplicate {
 
 	public static void main(String[] args) {
 		Set<String> aa = new HashSet<String>();
-		String[] m = { "ALL", "BLL", "CLL", "ALL", "ALL" };
+		String[] m = { "ALL", "BLL", "CLL", "ALL", "AALL" };
 		for (int i = 0; i < m.length; i++) {
 			if (!aa.add(m[i])) {
 				System.out.println(m[i]);
